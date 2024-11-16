4427398de8ee0202bb751a4b0bed9d64070f6c1c
Akshay9522-cell <akshaynamdev963@gmail.com>
2024-11-17 03:23:03 +0530

    navbar

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const para=document.getElementById("para")
+
+function myfunction(){
+    para.style.display='block'
+}
+function myfunction1(){
+    para.style.display='none'
+}
+
+const mousemove=document.getElementById("move")
+
+function myProject(){
+    mousemove.style.display='block'
+}
+function myProject1(){
+    mousemove.style.display='none'
+}
